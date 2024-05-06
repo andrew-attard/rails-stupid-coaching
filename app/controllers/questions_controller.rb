807b67779d90d17fae9df08619b6990140a10233
Andrew Attard <a2attard@gmail.com>
2024-05-06 13:44:30 +0900

    Added reference to a link that helped with understanding passing data between view/controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,9 @@ class QuestionsController < ApplicationController
   def ask
   end
 
+  # I was a bit confused about passing data from the controller to the view &
+  # from the view to the controller, but this article was useful:
+  # https://deku.posstree.com/en/ruby-on-rails/data-in-controller-view-route/#:~:text=It%20is%20easy%20to%20send,User%20sends%20Data%20via%20View
   def answer
     question = params[:question]
     if question == 'I am going to work right now!'
